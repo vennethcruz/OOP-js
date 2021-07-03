b9391dab2adaad79612885c020310ee75f17af37
Venneth <vennethcruz@gmail.com>
2021-07-04 00:46:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// const circle = {
+//     radius: 1,
+//     location: 1,
+//     draw: () => {
+//         console.log("hello asd")
+//     }
+// };
+
+// circle.draw()
+
+const createCircle = (radius) => {
+    return {
+        radius,
+        draw: () => {
+            console.log("draw funciton")
+            console.log(radius)
+        }
+    }
+}
+
+const circle = createCircle(1).draw()
+
+const Circle = (radius) => {
+    this.radius = radius
+    this.draw = () => {
+        console.log("draw this")
+    }
+}
+
+export const computeDistance = () => {
+    try {
+
+    } catch (error) {
+        console.log({ compute_distance_error: error })
+    }
+}
+
+const hello = new Circle(1)
